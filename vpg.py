044834aedee75b0e69ba59b4e511a21a65920adf
Nitish Pandey <nitishpandey2117@gmail.com>
2025-12-19 17:06:29 +0530

    update training loop

--- vpg.py
+++ vpg.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from dataclasses import dataclass
 
-# create a policy using function approximation
 
+# create a policy using function approximation
 # so let's say i have an environment
 # in that environment i will take an action
 # the current state of environment will be fed to the policy network
@@ -15,7 +15,6 @@ class TaskConfig:
     n_observation: int = 128
     n_hidden: int = 64
     n_action: int = 10
-
 
 class Policy(nn.Module):
     def __init__(self, cfg: TaskConfig) -> None:
@@ -34,28 +33,25 @@ class Policy(nn.Module):
         probs = self.head(logits)
         action = torch.argmax(probs, dim=-1)
         return action
-    
+
 
 env = None # assume you have the environment
 policy = Policy(TaskConfig())
 optimizer = torch.optim.AdamW(policy.parameters())
 
 # training loop
+# batch size 1, using r_t as training signal
 steps = 100
 for step in steps:
     # a single episode
     state = env.reset()
-    trajectory = []
+    cost = 0 # analogous to loss
     while True:
         action = policy(state) # sample action from policy
         next_state, reward, terminated = env.step(action) # perform action in environment
-        trajectory.append((state, action, reward, next_state))
+        cost += -action.log() * reward # add to cost
         if terminated:
             break
-    cost_function = 0
-    for transition in trajectory:
-        state, action, reward, next_state
-        cost_function += -action.log() * reward # add to cost 
     optimizer.zero_grad()
-    cost_function.backward()
+    cost.backward()
     optimizer.step()
