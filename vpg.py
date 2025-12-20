3c891c5344a0251c955429146326e9b60f9fd13e
Nitish Pandey <nitishpandey2117@gmail.com>
2025-12-20 18:49:31 +0530

    vpg: refactor training loop

--- vpg.py
+++ vpg.py
@@ -1,18 +1,11 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from dataclasses import dataclass
 import gymnasium as gym
+import torch.nn as nn
+import torch
+from torch.distributions import Bernoulli
 
-
-# create a policy using function approximation
-# so let's say i have an environment
-# in that environment i will take an action
-# the current state of environment will be fed to the policy network
-# policy network will output an action
 
 class Policy(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,):
         super().__init__()
         self.layers = nn.Sequential(
             nn.Linear(4, 48),
@@ -20,38 +13,59 @@ class Policy(nn.Module):
             nn.Linear(48, 48),
             nn.ReLU(),
             nn.Linear(48, 1),
+            nn.ReLU(),
             nn.Sigmoid()
         )
 
     def forward(self, obs: torch.Tensor) -> torch.Tensor:
-        prob = self.layers(obs)
-        action = torch.round(prob)
-        return action
+        probs = self.layers(obs)
+        return probs
 
 
 device = "cuda"
-env = gym.make("CartPole-v1")
-print(env.action_space)
-
+env = gym.make("CartPole-v1", render_mode="rgb_array")
 policy = Policy().to(device)
-optimizer = torch.optim.AdamW(policy.parameters())
+optimizer = torch.optim.AdamW(policy.parameters(), lr=1e-2)
 
-# training loop
-# batch size 1
-# using r_t as training signal
-steps = 1000
+
+steps = 500
 for step in range(steps):
-    # a single episode
+    # sample an episode
     observation, info = env.reset()
-    total_reward = torch.tensor([0.0], device=device) # analogous to loss
+
+    all_log_probs = []
+    all_rewards = []
     while True:
-        observation = torch.tensor(observation, device=device)
-        action = policy(observation) # sample action from policy
-        observation, reward, terminated, truncated, info = env.step(action) # perform action in environment
-        total_reward += reward * action.log() # TODO: improve the cost evaluator
+        obs = torch.as_tensor(observation, dtype=torch.float32, device=device)
+        probs = policy(obs)
+
+        # Q1
+        dist = Bernoulli(probs)
+        action = dist.sample()
+        log_prob = dist.log_prob(action)
+
+        next_observation, reward, terminated, truncated, info = env.step(action)
+        all_log_probs.append(log_prob)
+        all_rewards.append(reward)
+
         if terminated or truncated:
             break
+
+    # Q2
+    cost_value = 0.0
+    for i, log_prob in enumerate(all_log_probs):
+        cost_value += log_prob * all_rewards[i]
+    cost_value = torch.as_tensor(cost_value, dtype=torch.float32, device=device)
     optimizer.zero_grad()
-    total_reward.backward()
+    cost_value.backward()
     optimizer.step()
+
+
+
+
+
+
+
+# Q1. why bernoulli distribution ? why not directly infer the action using prob and do log to get log prob
+# Q2. is my way to calculate the final reward the correct way ? because that operation is not part of computation graph
 
