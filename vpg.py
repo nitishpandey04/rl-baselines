00acc80ecdb2c8baf9b778cda7e95d2abb95f9b1
Nitish Pandey <nitishpandey2117@gmail.com>
2025-12-21 18:36:46 +0530

    create ReinforceTrainer, resolve next_observation bug

--- vpg.py
+++ vpg.py
@@ -4,6 +4,7 @@ import gymnasium as gym
 import torch.nn as nn
 import torch
 
+
 class Policy(nn.Module):
     def __init__(self,):
         super().__init__()
@@ -20,88 +21,101 @@ class Policy(nn.Module):
         return logits
 
 
-device = "cuda"
-env = gym.make("CartPole-v1", render_mode="rgb_array")
-policy = Policy().to(device)
-optimizer = torch.optim.AdamW(policy.parameters(), lr=1e-2)
+class ReinforceTrainer:
+    def __init__(self, steps: int=30, batch_size: int=32, device: str="cuda"):
+        self.steps = steps
+        self.batch_size = batch_size
+        self.device = device
+        self.policy = Policy().to(device)
+        self.optimizer = torch.optim.AdamW(self.policy.parameters(), lr=1e-2)
+        self.env = gym.make("CartPole-v1", render_mode="rgb_array")
 
+    def train(self):
+        for step in range(self.steps):
+            # sample an episode
+            log_probs_batch = []
+            rewards_batch = []
 
-# TODO: batching, loss normalization, discount factor
-steps = 500
-batch_size = 32
-for step in range(steps):
-    # sample an episode
+            for i in range(self.batch_size):
+                observation, info = self.env.reset()
+                
+                log_probs = []
+                rewards = []
+                while True:
+                    obs = torch.as_tensor(observation, dtype=torch.float32, device=self.device)
+                    logits = self.policy(obs)
 
-    log_probs_batch = []
-    rewards_batch = []
+                    # bernoulli distribution because it makes sampling of log prob a differentiable operation
+                    # otherwise taking probability from sigmoid layer, doing log prob
+                    dist = Bernoulli(logits=logits)
+                    action = dist.sample()
+                    log_prob = dist.log_prob(action)
+                    action = action.to(torch.int32).item()
 
-    for i in range(batch_size):
+                    observation, reward, terminated, truncated, info = self.env.step(action)
+                    log_probs.append(log_prob)
+                    rewards.append(reward)
+
+                    if terminated or truncated:
+                        break
+
+                # compute rewards-to-go. TODO: add discount factor
+                for i in range(len(rewards) - 2, -1, -1):
+                    rewards[i] += rewards[i + 1]
+
+                log_probs_batch.extend(log_probs)
+                rewards_batch.extend(rewards)
+
+            # normalize rewards across batch
+            rewards_batch = torch.tensor(rewards_batch, device=self.device)
+            max_reward_per_batch = rewards_batch.max()
+            rewards_batch = (rewards_batch - rewards_batch.mean()) / (rewards_batch.std() + 1e-8)
+
+            # compute loss
+            log_probs_tensor = torch.stack(log_probs_batch).view(-1)
+            loss = -(log_probs_tensor * rewards_batch).sum()
+
+            # update policy
+            self.optimizer.zero_grad()
+            loss.backward()
+            grad_norm = torch.nn.utils.clip_grad_norm_(self.policy.parameters(), max_norm=1.0)
+            self.optimizer.step()
+
+            if step % 1 == 0:
+                print(f"Episode {step} | Loss {loss:.5f} | Grad norm {grad_norm:.5f} | Max reward per batch {max_reward_per_batch}")
+
+        self.save_policy()
+
+    def play(self):
+        # inference policy
+        env = gym.make("CartPole-v1", render_mode="human")
         observation, info = env.reset()
-        
-        log_probs = []
-        rewards = []
+        total_reward = 0
         while True:
-            obs = torch.as_tensor(observation, dtype=torch.float32, device=device)
-            logits = policy(obs)
-
-            dist = Bernoulli(logits=logits)
-            action = dist.sample()
-            log_prob = dist.log_prob(action)
-
-            next_observation, reward, terminated, truncated, info = env.step(action.to(torch.int32).item())
-            log_probs.append(log_prob)
-            rewards.append(reward)
-
+            obs = torch.as_tensor(observation, device=self.device, dtype=torch.float32)
+            logit = self.policy(obs)
+            prob = F.sigmoid(logit)
+            action = torch.round(prob).to(torch.int32).item()
+            observation, reward, terminated, truncated, info = env.step(action)
+            total_reward += reward
             if terminated or truncated:
                 break
+        env.close()
 
-        # compute rewards-to-go
-        for i in range(len(rewards) - 2, -1, -1):
-            rewards[i] += rewards[i + 1]
+    def save_policy(self, checkpoint_path="agent.pt"):
+        torch.save(self.policy.state_dict(), checkpoint_path)
 
-        log_probs_batch.extend(log_probs)
-        rewards_batch.extend(rewards)
+    def load_policy(self, checkpoint_path="agent.pt"):
+        state_dict = torch.load(checkpoint_path, weights_only=True)
+        self.policy.load_state_dict(state_dict)
+        
 
-    # normalize rewards across batch
-    rewards_batch = torch.tensor(rewards_batch, device=device)
-    max_reward_per_batch = rewards_batch.max()
-    rewards_batch = (rewards_batch - rewards_batch.mean()) / (rewards_batch.std() + 1e-8)
+trainer = ReinforceTrainer()
 
-    # compute loss
-    log_probs_tensor = torch.stack(log_probs_batch).view(-1)
-    loss = -(log_probs_tensor * rewards_batch).sum()
+# trainer.train()
 
-    # update policy
-    optimizer.zero_grad()
-    loss.backward()
-    grad_norm = torch.nn.utils.clip_grad_norm_(policy.parameters(), max_norm=1.0)
-    optimizer.step()
-
-    if step % 1 == 0:
-        print(f"Episode {step} | Loss {loss:.5f} | Grad norm {grad_norm:.5f} | Max reward per batch {max_reward_per_batch}")
-
-
-torch.save(policy.state_dict(), "agent.pt")
-
-
-# inference
-env = gym.make("CartPole-v1", render_mode="human")
-observation, info = env.reset()
-total_reward = 0
-while True:
-    obs = torch.as_tensor(observation, device=device, dtype=torch.float32)
-    logit = policy(obs)
-    prob = F.sigmoid(logit)
-    action = torch.round(prob).to(torch.int32).item()
-    observation, reward, terminated, truncated, info = env.step(action)
-    total_reward += reward
-    if terminated or truncated:
-        break
-env.close()
-
-
-
-
-
-# Q1. why bernoulli distribution ? why not directly infer the action using prob and do log to get log prob
-# Q2. is my way to calculate the final reward the correct way ? because that operation is not part of computation graph
+# before training
+trainer.play()
+trainer.load_policy()
+# after training
+trainer.play()
