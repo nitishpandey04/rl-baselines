98f796783013507eb3ea18f328ce210921a7d38d
Nitish Pandey <nitishpandey2117@gmail.com>
2025-12-26 11:46:35 +0530

    dqn: add replay buffer

--- dqn.py
+++ dqn.py
@@ -1,5 +1,7 @@
 import torch.nn.functional as F
+from collections import deque
 import torch.nn as nn
+import random
 import torch
 
 """
@@ -17,4 +19,16 @@ for n episodes:
 
 class ReplayBuffer:
     def __init__(self, size=100):
-        self.buffer = []
+        self.size = size
+        self.buffer = deque()
+
+    def insert(self, transition):
+        if len(self.buffer) == self.size:
+            self.buffer.popleft()
+        self.buffer.append(transition)
+
+    def sample(self, batch_size=32):
+        batch = random.sample(self.buffer, batch_size)
+        batch_tensor = torch.tensor(batch)
+        return batch_tensor
+
